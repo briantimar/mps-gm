dcc0e1f12f605b321330a25b2b4061da1d8e6903
Brian Timar <timarbrian@gmail.com>
2019-04-18 14:41:12 -0700

    svd and push with truncation working

--- test.py
+++ test.py
@@ -5,13 +5,24 @@ import numpy as np
 
 class TestUtils(TestCase):
 
-    def test_push_right(self):
+    def test_push_right_normalization(self):
         al = np.random.normal(size=(2,4,5)) + 1j*np.random.normal(size=(2,4,5))
         ar = np.random.normal(size=(2,5,6))
         al_new, ar_new = svd_push_right(al,ar)
        
         al_contracted = np.einsum('sij,sik->jk',al_new, np.conj(al_new))
         self.assertAlmostEqual(np.sum((al_contracted - np.identity(5))),0)
+    
+    def test_push_right_truncation(self):
+        al = np.random.normal(size=(2,4,5)) + 1j*np.random.normal(size=(2,4,5))
+        ar = np.random.normal(size=(2,5,6))
+        al_new, ar_new = svd_push_right(al,ar,max_sv_to_keep=2)
+        self.assertEqual(al_new.shape, (2, 4, 2))
+        self.assertEqual(ar_new.shape, (2, 2, 6))
+       
+        al_contracted = np.einsum('sij,sik->jk',al_new, np.conj(al_new))
+        self.assertAlmostEqual(np.sum((al_contracted - np.identity(2))),0)
+
 
     def test_push_left(self):
         al = np.random.normal(size=(2,4,5)) + 1j*np.random.normal(size=(2,4,5))
--- utils.py
+++ utils.py
@@ -1,25 +1,57 @@
 import numpy as np
 
-def svd_push_right(Aleft, Aright):
+
+def svd(A, cutoff=1e-8, max_sv_to_keep=None):
+    """ Perform singular value decomp of complex matrix A.
+        Singular values below cutoff will be dropped.
+        max_sv_to_keep: if not None, max number of singular values to keep
+        (the largest will be kept)
+
+        Returns:
+        u, s, v
+        where A \approx usv, A.shape=(N,M)
+        u.shape = (N,k)
+        s.shape = (k,)
+        v.shape = (k,M)
+        and k is the number of singular values retained.
+        """
+    u,s,v=np.linalg.svd(A, full_matrices=False)
+    singular_vals = np.diag(s)
+    #keep only singular values above cutoff
+    if cutoff:
+        singular_vals = singular_vals[singular_vals>cutoff]
+    #if a max number of eigvalues is requested, enforce that too
+    if max_sv_to_keep is not None:
+        singular_vals = singular_vals[:max_sv_to_keep]
+    #number of singular values
+    k=len(singular_vals)
+    #update the u and v matrices accordingly
+    u = u[:, :k]
+    v = v[:k, :]
+    return u, singular_vals, v
+
+def svd_push_right(Aleft, Aright, cutoff=1e-8, max_sv_to_keep=None):
     """Perform SVD on the left site matrix, and push singular values into the right site matrix.
         This results in the left site-matrix being left-normalized.
         Aleft: complex numpy array, shape (local_dim, bond_dim1, bond_dim2)
-        Aright: complex numpy array, shape (local_dim, bond_dim2, bond_dim3)"""
+        Aright: complex numpy array, shape (local_dim, bond_dim2, bond_dim3).
+        
+        Singular values below cutoff will be dropped.
+        max_sv_to_keep: if not None, max number of singular values to keep"""
 
     local_dim, D1, D2 = Aleft.shape
     # pull both indices over to the left side
     Aleft = np.reshape(Aleft, (local_dim * D1, D2))
     #svd on the D2 index
-    u,s,v=np.linalg.svd(Aleft, full_matrices=False)
-    #number of singular values
-    k=s.shape[0]
+    u,singular_vals, v = svd(Aleft, cutoff=cutoff,max_sv_to_keep=max_sv_to_keep)
+    k = len(singular_vals)
     #the unitary u defines the new left site matrix
     Aleft_new = np.reshape(u, (local_dim,D1,k))
     #contract s and v with the old right-matrix to define the new right-matrix
-    Aright_new = np.einsum('qi,sij->sqj',np.dot(np.diag(s), v),Aright)
+    Aright_new = np.einsum('q,qi,sij->sqj',singular_vals,v,Aright)
     return Aleft_new, Aright_new
     
-def svd_push_left(Aleft, Aright):
+def svd_push_left(Aleft, Aright, cutoff=1e-8, max_sv_to_keep=None):
     """Perform SVD on the right site matrix, and push singular values into the left site matrix.
         This results in the right site-matrix being right-normalized.
         Aleft: complex numpy array, shape (local_dim, bond_dim1, bond_dim2)
@@ -29,12 +61,12 @@ def svd_push_left(Aleft, Aright):
     #pull spin index to the right
     Aright = np.reshape(np.swapaxes(Aright, 0,1), (D2, local_dim * D3))
     #perform SVD on the D2-index
-    u,s,v = np.linalg.svd(Aright, full_matrices=False)
-    #number of singular values
-    k=s.shape[0]
+    u,singular_vals,v = svd(Aright, cutoff=cutoff,max_sv_to_keep=max_sv_to_keep)
+    k=len(singular_vals)
+  
     #new right-matrix, with shape local_dim, k, D3
     Aright_new = np.swapaxes(v.reshape( (k, local_dim, D3)), 0, 1)
     #push u and s to the left to get new left-matrix
-    Aleft_new = np.einsum('sij,jq->siq', Aleft,np.dot(u, np.diag(s)))
+    Aleft_new = np.einsum('sij,jq,q->siq', Aleft,u,singular_vals)
 
     return Aleft_new, Aright_new
