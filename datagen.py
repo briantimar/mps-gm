334fe514c723925b9c286665619f37df3346a799
Brian Timar <timarbrian@gmail.com>
2019-05-01 09:06:15 -0700

    removed datagen

--- datagen.py
+++
@@ -1,9 +0,0 @@
-def generate_samples(rho, N, how='complete'):
-    """ Generate samples from the state rho.
-        rho: a qutip state.
-        N: how many measurements to take 
-        how: a measurement strategy. Choices:
-            'complete': all 3^L pauli bases are used
-            'random': use random local rotations
-        """
-    pass
