51652fe260f6a0766702d2755e6d48836863b311
Brian Timar <timarbrian@gmail.com>
2019-06-27 11:01:38 -0400

    single-job format

--- experiments/fidelity-variation/fidelity-variation-001/do_training.sh
+++ experiments/fidelity-variation/fidelity-variation-001/do_training.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-OUTPUT_TEMPLATE="/home/btimar/slurm-logs/fidelity-variation-"
+OUTPUTTEMPLATE="/home/btimar/slurm-logs/fidelity-variation"
 
 source ./training_config.sh
 for name in "tfim_ground" "heisenberg_ground" "rydberg_ground" "ghz"; do
     for (( seed=0 ; seed<$NUMSEED ; seed=seed+1 )); do
-        OUTPUT="$OUTPUT_TEMPLATE$name_$seed"
-        sbatch -N 1 -n 1 --output $OUTPUT train_single.sh $name $SYSTEM_SIZE $BATCH_SIZE $seed &
+        OUTPUT="$OUTPUTTEMPLATE-$name-$seed"
+        sbatch -N 1 -n 1 --output=$OUTPUT train_single.sh $name $SYSTEM_SIZE $BATCH_SIZE $seed &
     done
 done
+echo "All jobs submitted"
+
+
